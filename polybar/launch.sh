97a0a19f944a0a569d79e8e9caebccf73ff36d1a
Marco Bompani <marcobompani91@gmail.com>
2021-06-09 23:39:35 +0100

    First commit

---
+++ polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use
+# polybar-msg cmd quit
+
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar HDMI1 >>/tmp/topbar.log 2>&1 &
+polybar eDP1 >>/tmp/topbar.log 2>&1 &
+# polybar bottom >>/tmp/bottombar.log 2>&1 &
+# polybar external >>/tmp/external.log 2>&1 &
+
+echo "Bars launched..."
